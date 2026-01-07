b55ac328035863447bef500ee5beefeb435183d3
Sambidhan26 <sambidhan26@gmail.com>
2026-01-07 11:31:03 +0545

    Adding Products: Before Migration Check

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace Bulky.DataAccess.Data
 
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -21,6 +22,46 @@ namespace Bulky.DataAccess.Data
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
                 );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Fortune of Time",
+                    Author = "Billy Spark",
+                    Description = "Praesent vitae sodales libero. ...",
+                    ISBN = "SWD9999001",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Dark Skies",
+                    Author = "Nancy Hoover",
+                    Description = "Praesent vitae sodales libero. ...",
+                    ISBN = "CAW777777701",
+                    ListPrice = 40,
+                    Price = 30,
+                    Price50 = 25,
+                    Price100 = 20
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Hello World",
+                    Author = "Computer",
+                    Description = "Programming Language",
+                    ISBN = "CAW777777702",
+                    ListPrice = 50,
+                    Price = 25,
+                    Price50 = 20,
+                    Price100 = 10
+                }
+            );
         }
+
     }
 }
