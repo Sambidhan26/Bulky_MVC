4a5bb80f7c13d7fb62c89fe54cf0566a36df6fd8
Sambidhan26 <sambidhan26@gmail.com>
2026-01-10 17:25:50 +0545

    Before migration caution

--- Bulky.Models/Models/Product.cs
+++ Bulky.Models/Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,5 +35,10 @@ namespace Bulky.Models.Models
         [Display(Name ="Price for 100+")]
         [Range(1,1000)]
         public double Price100 { get; set; }
+        public string ImageUrl { get; set; }
+        //Foreign Key
+        public int Id { get; set; }
+        [ForeignKey("Id")]
+        public Category Category { get; set; }
     }
 }
